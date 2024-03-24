59adc0da4768486f22cc464c10d13b9885336cb3
JuliaXmas <julia069910@gmail.com>
2024-03-24 15:55:56 +0300

    Created auth form and message

--- api.js
+++ api.js
@@ -1,6 +1,14 @@
+const commentsURL = "https://wedev-api.sky.pro/api/v2/alexandrova-julia/comments";
+const userURL = "https://wedev-api.sky.pro/api/user/login";
+
+const token = "asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+
 export function getComments() {
-    return fetch("https://wedev-api.sky.pro/api/v1/alexandrova-julia/comments", {
-        method: "GET"
+    return fetch(commentsURL, {
+        method: "GET",
+        headers: {
+            Authorization: token,
+        },
     })
         .then((response) => {
             if (response.status === 500) {
@@ -11,12 +19,30 @@ export function getComments() {
 }
 
 export function postComments({ name, text }) {
-    return fetch("https://wedev-api.sky.pro/api/v1/alexandrova-julia/comments", {
+    return fetch(commentsURL, {
         method: "POST",
+        headers: {
+            Authorization: token,
+        },
         body: JSON.stringify({
             name: name.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
             text: text.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
             forceError: true,
         }),
     })
+}
+
+export function login({ login, password }) {
+    return fetch(userURL, {
+        method: "POST",
+        body: JSON.stringify({
+            name: user.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
+            text: text.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
+            login,
+            password,
+            forceError: true,
+        }),
+    }).then((response) => {
+        return response.json();
+    });
 }
